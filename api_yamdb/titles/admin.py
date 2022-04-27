4cd94f3035f777cff7a1fb71ee39ef21840a150c
Алексей Юрасов <alex14ur@yandex.ru>
2022-04-27 21:56:14 +0300

    add admin panel

--- api_yamdb/titles/admin.py
+++ api_yamdb/titles/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Title, GenreTitle
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'category',
+    )
+    search_fields = ('name',)
+
+
+class GenreTitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title_id', 'genre_id')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(GenreTitle, GenreTitleAdmin)
